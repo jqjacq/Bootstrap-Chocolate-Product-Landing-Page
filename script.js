1609394ed5e4ee98e9d41be8192c68bfa209381d
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2022-05-25 15:15:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+tablebody = document.getElementById("tablebody")
+chocolates = [
+    {   type:"Milk Chocolate",
+        percent: "15-30%",
+        cost: "$4.99/lb,
+    } , {   
+        type:"White Chocolate",
+        percent: "0.5%",
+        cost: "$3.89/lb",
+    } , {   
+        type:"Dark Chocolate",
+        percent: "50%",
+        cost: "$5.49/lb",
+    } , {   
+        type:"Extra Dark Chocolate",
+        percent: "60%-70%",
+        cost: "$5.99/lb",
+    } , {   
+        type:"Ultra Dark Chocolate",
+        percent: "70%-85%",
+        cost: "$6.15/lb",
+    } , {   
+        type:"Mega Dark chocolates",
+        percent: "85%-95%",
+        cost: "$6.50/lb",
+    } , {   
+        type:"Customize your own",
+        percent: "0%-100%",
+        cost: "$8.99/lb",
+    }
+]
